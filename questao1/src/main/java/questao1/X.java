17e020720ac730d2261bad732307b9e6b3c1ef1f
jvcmtr <jvictorcmtr@gmail.com>
2025-06-18 17:30:39 -0300

    remove variavel temporaria

--- questao1/src/main/java/questao1/X.java
+++ questao1/src/main/java/questao1/X.java
@@ -27,7 +27,7 @@ public class X {
         } else {
             System.out.println("BAIXO");
         }
-        int temp = z * 0 + 42;
+        
         System.out.println("DEBUG: z = " + z);
         if (z > 10 && z > 5) { 
             System.out.println("ALTO");
