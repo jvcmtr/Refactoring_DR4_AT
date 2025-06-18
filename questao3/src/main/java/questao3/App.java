3dd6de9fce3dfe3fc43f0bda08f1f7e43eea5d02
jvcmtr <jvictorcmtr@gmail.com>
2025-06-18 19:35:02 -0300

    Extracted getDiscount Method

--- questao3/src/main/java/questao3/App.java
+++ questao3/src/main/java/questao3/App.java
@@ -15,10 +15,18 @@ public class App {
     }
 
     public static double calculatePrice(double basePrice, int customerType, boolean holiday) {
+        double discount = getDiscount(customerType, holiday);
+
+        return basePrice * (1 - discount);
+    }
+
+    public static double getDiscount(int customerType, boolean holiday) {
         double discount = 0;
+
         if (customerType == 1) discount = 0.1;
         else if (customerType == 2) discount = 0.15;
+        
         if (holiday) discount += 0.05;
-        return basePrice * (1 - discount);
+        return discount;
     }
 }
